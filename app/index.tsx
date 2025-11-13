e53c684b83350e2a52076916f4a7c026b5b57c2f
Happiesamuel <odionsamuel2005@gmail.com>
2025-11-13 16:56:39 +0100

    installed tilwind nd nativewind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 items-center justify-center  bg-red-500">
+      <Text className="text-5xl font-quicksand-light">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
