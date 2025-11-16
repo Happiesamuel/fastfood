b3ef105a8937d2e4f951197aee9c3a3849b1a9c4
Happiesamuel <odionsamuel2005@gmail.com>
2025-11-16 15:20:41 +0100

    created the routing

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Redirect, Slot } from "expo-router";
+import React from "react";
+
+export default function _layout() {
+  const isAuthenticated = false;
+  if (!isAuthenticated) return <Redirect href="/SignIn" />;
+  return <Slot />;
+}
